bbd23bcaaf44fe2ef45f0dbc78f3215299459b17
shutkin.90 <shutkin.90@mail.ru>
2022-10-16 21:15:11 +0400

    setup View Controllers

---
+++ Cocktails/CocktailCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CocktailCollectionViewCell.swift
+//  Cocktails
+//
+//  Created by Aleksandr on 16.10.2022.
+//
+
+import UIKit
+
+class CocktailCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var cocktailCellLabel: UILabel!
+    
+}
